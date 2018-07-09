7cbac0eb7d769666dcc7e649e774bc03b9392bda
Monologue <liangmingqing@modian.com>
2018-07-09 11:26:50 +0800

    update

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -14,4 +14,4 @@ new Vue({
   router,
   store,
   // el: '#app',
-}).$mount('router-view'); //等同于 el: '#app'  https://blog.csdn.net/c2311156c/article/details/80415633
+}).$mount('router-view'); //等同于 el: '#app'  https://segmentfault.com/a/1190000009467029
