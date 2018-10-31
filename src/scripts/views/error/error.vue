2168d8966b3d6902bacffce12f695f0b1a54ba30
Monologue <liangmingqing@modian.com>
2018-10-31 11:30:31 +0800

    add server error

---
+++ src/scripts/views/error/error.vue
@@ -0,0 +1,24 @@
+<template>
+  <div style="font-size: 30px">
+    <p>NOT FOUND</p>
+  </div>
+</template>
+
+<script>
+  exports.default = {
+    data() {
+
+    },
+    computed: {
+
+    },
+    methods: {
+
+    },
+    components: {
+
+    },
+    mounted() {
+    }
+  }
+</script>
---
+++ src/scripts/views/error/route.js
@@ -0,0 +1,6 @@
+import error from './error.vue';
+
+export default {
+    path: '*',
+    component: error,
+}
