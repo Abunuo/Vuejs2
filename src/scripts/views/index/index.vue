84b3cee6106473a55a3a21205753571d277f64fe
Monologue <liangmingqing@modian.com>
2019-10-28 18:53:41 +0800

    longpress

--- src/scripts/views/index/index.vue
+++ src/scripts/views/index/index.vue
@@ -1,23 +1,3 @@
-<template>
-	<div id="container">
-		<p>{{ total }}</p>
-		<button-counter v-autoSub v-on:increment="incrementTotal"></button-counter>
-		<button-counter v-on:increment="incrementTotal" v-bind:name="a"></button-counter>
-		<button-counter v-on:increment="incrementTotal" v-bind:age="'我是第二个'" />
-		<router-link ref="goTo" to="/second">点我跳转第二个</router-link>
-		<br>
-		<button @click="show = !show" v-bind:style="{color:'red', fontSize:'14px', width:'auto', lineHeight:'30px', marginTop:'.2rem'}"> 点击我看动画 </button>
-		<transition name="slide-fade">
-			<p v-show="show">hello</p>
-		</transition>
-		<p v-if="show" transition="fade">
-			Molonogue
-		</p>
-		<my-compontent :value="123">abunuo</my-compontent>
-		<div id="gradient" @click="staticUpdate"></div>
-	</div>
-</template>
-
 <style scoped>
 	.slide-fade-enter-active {
 		transition: all .3s ease;
@@ -33,6 +13,26 @@
 		opacity: 0;
 	}
 </style>
+
+<template>
+	<div id="container">
+		<p>{{ total }}</p>
+		<button-counter v-autoSub v-on:increment="incrementTotal"></button-counter>
+		<button-counter v-on:increment="incrementTotal" v-bind:name="a"></button-counter>
+		<button-counter v-on:increment="incrementTotal" v-bind:age="'我是第二个'" />
+		<router-link ref="goTo" to="/second">点我跳转第二个</router-link>
+		<br>
+		<button @click="show = !show" v-bind:style="{color:'red', fontSize:'14px', width:'auto', lineHeight:'30px', marginTop:'.2rem'}"> 点击我看动画 </button>
+		<transition name="slide-fade">
+			<p v-show="show">hello</p>
+		</transition>
+		<p v-if="show" transition="fade" v-longpress="showAT(longPressData)">
+			Molonogue
+		</p>
+		<my-compontent :value="123">abunuo</my-compontent>
+		<div id="gradient" @click="staticUpdate"></div>
+	</div>
+</template>
 
 <script>
     import Debug from 'debug';
@@ -52,6 +52,10 @@
     			a: '我是第一个',
     			show: true,
 				staticData: 1,  //视图层未使用的数据更新不会触发视图层更新，故不存在 updated 周期的触发
+				longPressData: {
+					content: '长按文案',
+					id: 123
+				}
     		}
     	},
     	created() {
@@ -76,7 +80,64 @@
     			inserted: (el) => {
     				el.click();
     			}
-    		}
+    		},
+			/*
+				长按指令
+				eg. v-longpress:[{content:content,reply_id:reply_id}]="longtap"
+			 */
+			'longpress': {
+                bind: function (el, binding, vNode) {
+                    // Make sure expression provided is a function
+                    if (typeof binding.value !== 'function') {
+                        // Fetch name of component
+                        const compName = vNode.context.name
+                        // pass warning to console
+                        let warn = `[longpress:] provided expression '${binding.expression}' is not a function, but has to be`
+                        if (compName) { warn += `Found in component '${compName}' ` }
+
+                        console.warn(warn)
+                    }
+                    // Define variable
+                    let pressTimer = null
+                    
+                    // Define funtion handlers
+                    // Create timeout ( run function after 1s )
+                    let start = (e) => {
+                        if (e.type === 'click' && e.button !== 0) {
+                            return;
+                        }
+
+                        if (pressTimer === null) {
+                            pressTimer = setTimeout(() => {
+                                // Run function
+                                handler()
+                            }, 500)
+                        }
+                    }
+
+                    // Cancel Timeout
+                    let cancel = (e) => {
+                        // Check if timer has a value or not
+                        if (pressTimer !== null) {
+                            clearTimeout(pressTimer)
+                            pressTimer = null
+                        }
+                    }
+                    // Run Function
+                    const handler = () => {
+                        binding.value()
+                    }
+
+                    // Add Event listeners
+                    el.addEventListener("mousedown", start, { passive: false });
+                    el.addEventListener("touchstart", start, { passive: false });
+                    // Cancel timeouts if this events happen
+                    el.addEventListener("click", cancel, { passive: false });
+                    el.addEventListener("mouseout", cancel, { passive: false });
+                    el.addEventListener("touchend", cancel, { passive: false });
+                    el.addEventListener("touchcancel", cancel, { passive: false });
+                }
+            },
     	},
     	methods: { //事件方法
     		incrementTotal: function() {
@@ -84,6 +145,11 @@
     		},
 			staticUpdate: function(){
 				this.staticData ++;
+			},
+			showAT(longPressData) {
+				return () => {
+					console.log(longPressData.content, longPressData.id)
+				}
 			}
     	},
     	updated() {
